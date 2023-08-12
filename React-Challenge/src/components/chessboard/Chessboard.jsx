4c8bba48c871e13d9f311c6549f6b70d7063ebdf
khingbob <bobur.khayitov2003@gmail.com>
2023-08-12 15:23:48 +0200

    Initial setup

--- React-Challenge/src/components/chessboard/Chessboard.jsx
+++ React-Challenge/src/components/chessboard/Chessboard.jsx
@@ -1,11 +1,30 @@
 import css from "./Chessboard.module.css";
 import Square from "../square/Square";
 /* The chessboard will contain all the squares */
+/* The coordinate system starts from the bottom left with 0 */
 const Chessboard = () => {
-  return (
-    <div id={css.chessboard}>
-      <Square color="black" />
-    </div>
-  );
+  // Coordinates of figures
+  let board = [];
+  // Initial setup
+  for (let x = 0; x < 8; x++) {
+    let column = [];
+    let a = x % 2 == 0 ? "white" : "gray";
+    let b = x % 2 == 0 ? "gray" : "white";
+    for (let y = 0; y < 8; y++) {
+      column.push(
+        <Square
+          key={x + "|" + y}
+          //color of the square
+          color={y % 2 == 0 ? a : b}
+          figure={
+            (y == 1 && (x < 4 ? "wqueen" : "wknight")) ||
+            (y == 6 && (x < 4 ? "bqueen" : "bknight"))
+          }
+        />
+      );
+    }
+    board.push(column);
+  }
+  return <div id={css.chessboard}>{board}</div>;
 };
 export default Chessboard;
--- React-Challenge/src/components/square/Square.jsx
+++ React-Challenge/src/components/square/Square.jsx
@@ -1,12 +1,24 @@
 import css from "./Square.module.css";
-// A square can have a color, contain the chess pieces, and show movement symbols
+import wknight from "../../assets/wknight.png";
+import bknight from "../../assets/bknight.png";
+import wqueen from "../../assets/wqueen.png";
+import bqueen from "../../assets/bqueen.png";
+// A square can have a color, contain the chess figures, and show movement symbols
 const Square = (props) => {
+  let figures = {
+    wknight: wknight,
+    bknight: bknight,
+    wqueen: wqueen,
+    bqueen: bqueen,
+  };
   return (
     <div
       className={`${css.square} ${
-        props.color == "black" ? css.black : css.white
+        props.color == "gray" ? css.gray : css.white
       }`}
-    ></div>
+    >
+      <img src={figures[props.figure]} className={css.figure} />
+    </div>
   );
 };
 export default Square;
