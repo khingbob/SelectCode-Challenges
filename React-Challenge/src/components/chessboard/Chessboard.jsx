eb730b59bd31f34e519212bcf66d59d02ca097e5
khingbob <bobur.khayitov2003@gmail.com>
2023-08-15 11:47:37 +0200

    selectable square

--- React-Challenge/src/components/chessboard/Chessboard.jsx
+++ React-Challenge/src/components/chessboard/Chessboard.jsx
@@ -1,30 +1,40 @@
 import css from "./Chessboard.module.css";
 import Square from "../square/Square";
+import { useEffect, useState } from "react";
 /* The chessboard will contain all the squares */
 /* The coordinate system starts from the bottom left with 0 */
 const Chessboard = () => {
-  // Coordinates of figures
-  let board = [];
-  // Initial setup
-  for (let x = 0; x < 8; x++) {
-    let column = [];
-    let a = x % 2 == 0 ? "white" : "gray";
-    let b = x % 2 == 0 ? "gray" : "white";
-    for (let y = 0; y < 8; y++) {
-      column.push(
-        <Square
-          key={x + "|" + y}
-          //color of the square
-          color={y % 2 == 0 ? a : b}
-          figure={
-            (y == 1 && (x < 4 ? "wqueen" : "wknight")) ||
-            (y == 6 && (x < 4 ? "bqueen" : "bknight"))
-          }
-        />
-      );
+  // Coordinates of figures board[x][y]
+  // When squares inside of the board change, they get rerendered
+  const [board, setBoard] = useState([]);
+
+  useEffect(() => {
+    // Initial setup
+    let tempBoard = [];
+    for (let x = 0; x < 8; x++) {
+      let column = [];
+      let a = x % 2 == 0 ? "white" : "gray";
+      let b = x % 2 == 0 ? "gray" : "white";
+      for (let y = 0; y < 8; y++) {
+        column.push(
+          <Square
+            board={board}
+            setBoard={setBoard}
+            key={x + "|" + y}
+            id={x + "|" + y}
+            //color of the square
+            color={y % 2 == 0 ? a : b}
+            figure={
+              (y == 1 && (x < 4 ? "wqueen" : "wknight")) ||
+              (y == 6 && (x < 4 ? "bqueen" : "bknight"))
+            }
+          />
+        );
+      }
+      tempBoard.push(column);
     }
-    board.push(column);
-  }
+    setBoard(tempBoard);
+  }, []);
   return <div id={css.chessboard}>{board}</div>;
 };
 export default Chessboard;
--- React-Challenge/src/components/square/Square.jsx
+++ React-Challenge/src/components/square/Square.jsx
@@ -3,21 +3,53 @@ import wknight from "../../assets/wknight.png";
 import bknight from "../../assets/bknight.png";
 import wqueen from "../../assets/wqueen.png";
 import bqueen from "../../assets/bqueen.png";
-// A square can have a color, contain the chess figures, and show movement symbols
+import { useState } from "react";
+/* A square gets passed the board, it can have a color, contain the chess figures, and show movement symbols */
 const Square = (props) => {
+  //current square color
+  const [color, setColor] = useState(props.color);
+  //initial square color
+  const squareColor = props.color;
+  //board state management
+  const board = props.board;
+  const setBoard = props.setBoard;
+
+  const id = props.id;
+  const figure = props.figure;
+
   let figures = {
     wknight: wknight,
     bknight: bknight,
     wqueen: wqueen,
     bqueen: bqueen,
   };
+  let colors = {
+    gray: css.gray,
+    white: css.white,
+    selected: css.selected,
+  };
+  //click outside of this square
+  document.addEventListener("click", (e) => {
+    if (!document.getElementById(id).contains(e.target)) {
+      setColor(squareColor);
+    }
+  });
+  const squareClick = () => {
+    let x = parseInt(props.id.split("|")[0]);
+    let y = parseInt(props.id.split("|")[1]);
+    figure && setColor("selected");
+  };
+
   return (
     <div
-      className={`${css.square} ${
-        props.color == "gray" ? css.gray : css.white
-      }`}
+      className={`${css.square} ${colors[color]}`}
+      id={id}
+      onClick={squareClick}
+      onBlur={() => {
+        setColor("white");
+      }}
     >
-      <img src={figures[props.figure]} className={css.figure} />
+      <img src={figures[figure]} className={css.figure} />
     </div>
   );
 };
