d54811c08de64ff45ea7fcd76b97151a54937ccc
khingbob <bobur.khayitov2003@gmail.com>
2023-08-17 08:58:08 +0200

    knight directions

--- React-Challenge/src/components/chessboard/Chessboard.jsx
+++ React-Challenge/src/components/chessboard/Chessboard.jsx
@@ -1,40 +1,57 @@
 import css from "./Chessboard.module.css";
 import Square from "../square/Square";
 import { useEffect, useState } from "react";
-/* The chessboard will contain all the squares */
-/* The coordinate system starts from the bottom left with 0 */
+/* The chessboard will contain all the squares
+  The coordinate system starts from the bottom left with 0 */
 const Chessboard = () => {
-  // Coordinates of figures board[x][y]
-  // When squares inside of the board change, they get rerendered
+  // Coordinates of squares: board[x][y]
+  // When squares inside of the board array change, it rerenders
   const [board, setBoard] = useState([]);
+
+  // The last clicked square. Used for clean-up purposes
+  const [clicked, setClicked] = useState(null);
 
   useEffect(() => {
     // Initial setup
     let tempBoard = [];
     for (let x = 0; x < 8; x++) {
-      let column = [];
+      let row = [];
       let a = x % 2 == 0 ? "white" : "gray";
       let b = x % 2 == 0 ? "gray" : "white";
       for (let y = 0; y < 8; y++) {
-        column.push(
-          <Square
-            board={board}
-            setBoard={setBoard}
-            key={x + "|" + y}
-            id={x + "|" + y}
-            //color of the square
-            color={y % 2 == 0 ? a : b}
-            figure={
-              (y == 1 && (x < 4 ? "wqueen" : "wknight")) ||
-              (y == 6 && (x < 4 ? "bqueen" : "bknight"))
-            }
-          />
-        );
+        row.push({
+          key: x + "|" + y,
+          id: x + "|" + y,
+          //color of the square
+          squareColor: y % 2 == 0 ? a : b,
+          figure:
+            (y == 1 && (x < 4 ? "wqueen" : "wknight")) ||
+            (y == 6 && (x < 4 ? "bqueen" : "bknight")) ||
+            null,
+        });
       }
-      tempBoard.push(column);
+      tempBoard.push(row);
     }
     setBoard(tempBoard);
   }, []);
-  return <div id={css.chessboard}>{board}</div>;
+  return (
+    <div id={css.chessboard}>
+      {board.map((rows) =>
+        rows.map((data) => (
+          <Square
+            board={board}
+            setBoard={setBoard}
+            key={data.key}
+            id={data.id}
+            squareColor={data.squareColor}
+            highlight={data.highlight}
+            figure={data.figure}
+            clicked={clicked}
+            setClicked={setClicked}
+          />
+        ))
+      )}
+    </div>
+  );
 };
 export default Chessboard;
--- React-Challenge/src/components/square/Square.jsx
+++ React-Challenge/src/components/square/Square.jsx
@@ -3,53 +3,145 @@ import wknight from "../../assets/wknight.png";
 import bknight from "../../assets/bknight.png";
 import wqueen from "../../assets/wqueen.png";
 import bqueen from "../../assets/bqueen.png";
-import { useState } from "react";
-/* A square gets passed the board, it can have a color, contain the chess figures, and show movement symbols */
+import dot from "../../assets/dot.png";
+import { useEffect, useState } from "react";
+/* A square gets passed the board, it can have a color and a highlight, contain the chess figures, and show movement symbols */
 const Square = (props) => {
-  //current square color
-  const [color, setColor] = useState(props.color);
-  //initial square color
-  const squareColor = props.color;
   //board state management
-  const board = props.board;
+  //props.board is being spread to avoid mutation
+  const board = [...props.board];
   const setBoard = props.setBoard;
 
+  //clicked square state
+  const clicked = props.clicked;
+  const setClicked = props.setClicked;
+  const cx = clicked ? clicked[0] : null;
+  const cy = clicked ? clicked[1] : null;
+
+  //id of the square container
   const id = props.id;
+
+  //coordinates of the square
+  const x = parseInt(props.id.split("|")[0]);
+  const y = parseInt(props.id.split("|")[1]);
+
+  //the figure on the square
   const figure = props.figure;
 
-  let figures = {
-    wknight: wknight,
-    bknight: bknight,
-    wqueen: wqueen,
-    bqueen: bqueen,
+  //square highlight
+  const highlight = props.highlight;
+
+  //square color
+  const squareColor = props.squareColor;
+
+  /* object matching the figure prop with the imported variable  */
+  const figures = {
+    wknight,
+    bknight,
+    wqueen,
+    bqueen,
+    dot,
   };
-  let colors = {
+
+  // matching the square color prop with the class-name of the modular css component
+  const squareColors = {
     gray: css.gray,
     white: css.white,
-    selected: css.selected,
   };
-  //click outside of this square
-  document.addEventListener("click", (e) => {
-    if (!document.getElementById(id).contains(e.target)) {
-      setColor(squareColor);
+  // matching the highlight prop with the class-name of the modular css component
+  const highlights = {
+    selected: css.selected,
+    attack: css.attack,
+  };
+
+  /* || CLEANERS */
+
+  //removes the dots that the knight was showing when selected
+  const knightDotsCleaner = (x, y) => {
+    for (let xd = -2; xd <= 2; xd++) {
+      if (xd == 0) continue;
+      for (let yd = -2; yd <= 2; yd++) {
+        if (Math.abs(Math.abs(xd) - Math.abs(yd)) != 1 || yd == 0) continue;
+        if (x + xd >= 0 && x + xd < 8 && y + yd >= 0 && y + yd < 8) {
+          board[x + xd][y + yd] = {
+            key: x + xd + "|" + (y + yd),
+            id: x + xd + "|" + (y + yd),
+            squareColor: board[x + xd][y + yd].squareColor,
+            highlight: null,
+            // if the aim was a dot it has to be cleaned, otherwise the figure should stay
+            figure:
+              board[x + xd][y + yd].figure == "dot"
+                ? null
+                : board[x + xd][y + yd].figure,
+          };
+        }
+      }
     }
-  });
+  };
+
+  // matching cleaners with the figures
+  const cleaners = {
+    bknight: knightDotsCleaner,
+    wknight: knightDotsCleaner,
+    // bqueen:queenDotsCleaner, wqueen: queenDotsCleaner,
+  };
+
+  /* || DIRECTIONS */
+
+  //available movements for a knight
+  const knightDirection = () => {
+    for (let xd = -2; xd <= 2; xd++) {
+      if (xd == 0) continue;
+      for (let yd = -2; yd <= 2; yd++) {
+        if (Math.abs(Math.abs(xd) - Math.abs(yd)) != 1 || yd == 0) continue;
+        if (x + xd >= 0 && x + xd < 8 && y + yd >= 0 && y + yd < 8) {
+          board[x + xd][y + yd] = {
+            key: x + xd + "|" + (y + yd),
+            id: x + xd + "|" + (y + yd),
+            squareColor: board[x + xd][y + yd].squareColor,
+            figure: board[x + xd][y + yd].figure ?? "dot",
+            highlight: board[x + xd][y + yd].figure
+              ? highlights["attack"]
+              : null,
+          };
+        }
+      }
+    }
+  };
+
+  const directions = {
+    wknight: knightDirection,
+    bknight: knightDirection,
+    // wqueen: queenDirection, bqueen: queenDirection
+  };
+
+  // Click-handler on a square
   const squareClick = () => {
-    let x = parseInt(props.id.split("|")[0]);
-    let y = parseInt(props.id.split("|")[1]);
-    figure && setColor("selected");
+    // cleaning if a figure was selected before
+    if (clicked && board[cx][cy].figure) {
+      cleaners[board[cx][cy].figure](cx, cy);
+      board[cx][cy] = { ...board[cx][cy], highlight: null };
+    }
+    // giving a highlight if a figure was selected now
+    if (figure && figure != "dot")
+      board[x][y] = { ...board[x][y], highlight: "selected" };
+    //showing the available movements for this figure
+    figure && figure != "dot" && directions[figure]();
+    //rerendering changes
+    setBoard(board);
+    //updating the last clicked square
+    setClicked([x, y]);
   };
 
   return (
     <div
-      className={`${css.square} ${colors[color]}`}
+      className={`${css.square} ${squareColors[squareColor]}`}
       id={id}
       onClick={squareClick}
-      onBlur={() => {
-        setColor("white");
-      }}
     >
-      <img src={figures[figure]} className={css.figure} />
+      <div className={`${css.highlight} ${highlights[highlight]}`}>
+        <img src={figures[figure]} className={css.figure} />
+      </div>
     </div>
   );
 };
